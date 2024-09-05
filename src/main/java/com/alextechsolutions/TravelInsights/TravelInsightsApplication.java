2feb0476735dacb24ab4d9e1eaa336fbf9570001
GauravEngineer <gauravkumar.engr@gmail.com>
2024-09-05 21:43:01 +0530

    Added Project

---
+++ src/main/java/com/alextechsolutions/TravelInsights/TravelInsightsApplication.java
@@ -0,0 +1,13 @@
+package com.alextechsolutions.TravelInsights;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelInsightsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelInsightsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alextechsolutions/TravelInsights/TravelInsightsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alextechsolutions.TravelInsights;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelInsightsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
