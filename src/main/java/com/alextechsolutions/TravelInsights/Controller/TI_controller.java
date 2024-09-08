028ad11a8076b30d4225f81afefe51167e61525e
Sajan-Kumar-Sahu <saajansahu.3007@gmail.com>
2024-09-08 09:26:05 +0530

    Contact page has been Integrated

--- src/main/java/com/alextechsolutions/TravelInsights/Controller/TI_controller.java
+++ src/main/java/com/alextechsolutions/TravelInsights/Controller/TI_controller.java
@@ -1,8 +1,14 @@
 package com.alextechsolutions.TravelInsights.Controller;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @RestController
 public class TI_controller {
-    
+    @GetMapping("/contact")
+    public RedirectView getContactPage() {
+        return new RedirectView("/html/contact.html");
+    }
 }
